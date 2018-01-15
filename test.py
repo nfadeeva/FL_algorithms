2c39aa2c092ace5d7cbbbca52ec4d591c57dc4e1
nfadeeva <fadeevanastasiia@gmail.com>
2018-01-15 15:55:04 +0300

    Update tests

--- test.py
+++ test.py
@@ -13,13 +13,15 @@ Q1_ = True
 Q2_ = True
 
 
-def helper_function(grammar_hom, grammar_automata, graph_name, right_result):
+def helper_function(grammar_hom, grammar_automata, graph_name, right_result,
+                    func_hom = lambda x: set(filter(lambda x: x[1]=='S',x[0])) == x[1],
+                    func_automata = lambda x: set(x[0])==x[1]):
     G_hom = parse_grammar_hom(grammar_hom)
     G_automata = parse_grammar_automata(grammar_automata)
 
     if TRANS_CLOSURE:
         result_closure = trans_closure(parse_graph(graph_name), G_hom)
-        assert set(filter(lambda x: x[1]=='S',result_closure)) == right_result
+        assert func_hom((result_closure, right_result))
         print("test for {grammar_hom} and {graph_name}"
               " - trans_closure - OK"
               .format(grammar_hom=os.path.basename(grammar_hom),
@@ -28,7 +30,7 @@ def helper_function(grammar_hom, grammar_automata, graph_name, right_result):
 
     if BOTTOM_UP:
         result_bottom_up = bottom_up(parse_graph(graph_name), G_automata)
-        assert set(result_bottom_up) == right_result
+        assert func_automata((result_bottom_up,right_result))
         print("test for {grammar_automata} and {graph_name}"
               " - bottom_up - OK"
               .format(grammar_automata=os.path.basename(grammar_automata),
@@ -37,7 +39,7 @@ def helper_function(grammar_hom, grammar_automata, graph_name, right_result):
 
     if GLL:
         result_gll = gll(parse_graph(graph_name), G_automata)
-        assert set(result_gll) == right_result
+        assert func_automata((result_gll,right_result))
         print("test for {grammar_automata} and {graph_name}"
               " - gll - OK"
               .format(grammar_automata=os.path.basename(grammar_automata),
@@ -56,7 +58,8 @@ def test_my_example_0():
     grammar_hom = "data/grammars/grammar_hom_0"
     grammar_automata = "data/grammars/grammar_automata_0"
     graph_name = "data/graphs/graph_0"
-    helper_function(grammar_hom, grammar_automata, graph_name, right_result)
+    helper_function(grammar_hom, grammar_automata, graph_name, right_result,
+                    func_hom = lambda x: set(x[0])==x[1])
 
 
 # grammar with cycle and the same graph (1^*2)
@@ -68,12 +71,11 @@ def test_my_example_1():
     helper_function(grammar_hom, grammar_automata, graph_name, right_result)
 
 
-# grammar with cycle and linear graph
+# grammar with cycle and linear graph (S-> S S S, S-> S S, S->a + a->a->a->a)
 def test_my_example_2():
-    right_result = {(0, 'S', 1), (0, 'S', 2),(0, 'S', 3),(0, 'S', 4),
-                    (1, 'S', 2), (1, 'S', 3),(1, 'S', 4),
-                    (2, 'S', 3),(2, 'S', 4),
-                    (3, 'S', 4)
+    right_result = {(0, 'S', 1), (0, 'S', 2),(0, 'S', 3),
+                    (1, 'S', 2), (1, 'S', 3),
+                    (2, 'S', 3)
                     }
     grammar_hom = "data/grammars/grammar_hom_3"
     grammar_automata = "data/grammars/grammar_automata_3"
@@ -82,11 +84,10 @@ def test_my_example_2():
 
 # grammar with cycle and graph with the cycle
 def test_my_example_4():
-    right_result = {(0, 'S', 0),(0, 'S', 1), (0, 'S', 2),(0, 'S', 3),(0, 'S', 4),
-                    (1, 'S', 0), (1, 'S', 1),(1, 'S', 2), (1, 'S', 3),(1, 'S', 4),
-                    (2, 'S', 0), (2, 'S', 1), (2, 'S', 2), (2, 'S', 3), (2, 'S', 4),
-                    (3, 'S', 0), (3, 'S', 1), (3, 'S', 2), (3, 'S', 3), (3, 'S', 4),
-                    (4, 'S', 0), (4, 'S', 1), (4, 'S', 2), (4, 'S', 3), (4, 'S', 4),
+    right_result = {(0, 'S', 0),(0, 'S', 1), (0, 'S', 2),(0, 'S', 3),
+                    (1, 'S', 0), (1, 'S', 1),(1, 'S', 2), (1, 'S', 3),
+                    (2, 'S', 0), (2, 'S', 1), (2, 'S', 2), (2, 'S', 3),
+                    (3, 'S', 0), (3, 'S', 1), (3, 'S', 2), (3, 'S', 3)
 
                     }
     grammar_hom = "data/grammars/grammar_hom_3"
@@ -94,12 +95,22 @@ def test_my_example_4():
     graph_name = "data/graphs/graph_3"
     helper_function(grammar_hom, grammar_automata, graph_name, right_result)
 
-# grammar_1 with eps
+# grammar a^nb^n and graph with to coprime cycles
+def test_my_example_5():
+    right_result = (2, 'S', 2)
+    grammar_hom = "data/grammars/grammar_hom_4"
+    grammar_automata = "data/grammars/grammar_automata_4"
+    graph_name = "data/graphs/graph_4"
+    helper_function(grammar_hom, grammar_automata, graph_name, right_result,
+                    func_hom=lambda x: x[1] in x[0],
+                    func_automata = lambda x: x[1] in x[0])
+
+# grammar with eps
 def test_my_example_3():
     right_result = {(0, 'S', 0), (0, 'S', 1),(1, 'S', 1)}
     grammar_hom = "data/grammars/grammar_hom_2"
     grammar_automata = "data/grammars/grammar_automata_2"
-    graph_name = "data/graphs/graph_1"
+    graph_name = "data/graphs/graph_5"
     helper_function(grammar_hom, grammar_automata, graph_name, right_result)
 
 
